ee74c3df82c3e9fa6d3bc9e897e946be940690c9
Gabriel Vitório Gonçalves <104917512+GabrielVitorioGoncalves@users.noreply.github.com>
2025-04-02 15:29:29 -0300

    css organizado
    Os css estão mais organizados agora

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
-
+ // Não sei oque fiz aqui mais por algum motivo so carrega a pagina de login se estiver assim
+router.get('/login.ejs', function(req,res,next){
+  res.render('login');
+});
 module.exports = router;
