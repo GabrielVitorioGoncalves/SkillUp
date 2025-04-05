8b0679c43f63eefea5a60bebe87b423fed819de4
Lucas05-Oliveira <lucas.l.oliveira0509@gmail.com>
2025-04-05 17:59:45 -0300

    controle de admin
    login de admin funcionando// atualizei banco de dados

--- banco.js
+++ banco.js
@@ -31,7 +31,7 @@ async function buscarUsuario(usuario) {
   const conex = await conectarBD();
 
   const sql = "select * from usuarios where usu_email=? and usu_senha=?";
-  
+
   const [usuarioEncontrado] = await conex.query(sql, [usuario.email, usuario.senha]);
 
   if (usuarioEncontrado && usuarioEncontrado.length > 0) { //tamanho maior q 0
@@ -41,8 +41,20 @@ async function buscarUsuario(usuario) {
   }
 }
 
+async function buscarAdmin(admin){
+  const conex = await conectarBD();
 
+  const sql= "select * from admin where adm_email=? and adm_senha=?";
+
+  const [admEncontrado] = await conex.query(sql, [admin.email, admin.senha]);
+
+  if(admEncontrado && admEncontrado.length > 0){
+    return admEncontrado[0];
+  }else{
+    return {};
+  }
+};
 
 conectarBD();
 
-module.exports = {buscarUsuario}
+module.exports = {buscarUsuario, buscarAdmin}
